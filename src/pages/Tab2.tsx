6e2bdc44cf41a05ddc6d0ec17d685910b46328a5
pandaindisguise <pandaindisguise@gmail.com>
2022-01-05 03:28:18 -0600

    add usePhotoGallery hook

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -16,6 +16,7 @@ import {
 } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
 
 const Tab2: React.FC = () => {
   return (
